bfb38fb55ad19788e6cfda048668a2feacf29464
ramgopalsiddh <ramgopalsiddh.000@gmail.com>
2022-10-06 15:37:21 +0530

    js code update 2

--- calculator.js
+++ calculator.js
@@ -37,3 +37,60 @@ function handleMath(value) {
 
   buffer = "0";
 }
+function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+      runningTotal += intBuffer;
+    } else if (previousOperator === "-") {
+      runningTotal -= intBuffer;
+    } else if (previousOperator === "×") {
+      runningTotal *= intBuffer;
+    } else {
+      runningTotal /= intBuffer;
+    }
+  }
+  
+  function handleSymbol(value) {
+    switch (value) {
+      case "C":
+        buffer = "0";
+        runningTotal = 0;
+        break;
+      case "=":
+        if (previousOperator === null) {
+          // need two numbers to do math
+          return;
+        }
+        flushOperation(parseInt(buffer));
+        previousOperator = null;
+        buffer = +runningTotal;
+        runningTotal = 0;
+        break;
+      case "←":
+        if (buffer.length === 1) {
+          buffer = "0";
+        } else {
+          buffer = buffer.substring(0, buffer.length - 1);
+        }
+        break;
+      case "+":
+      case "-":
+      case "×":
+      case "÷":
+        handleMath(value);
+        break;
+    }
+  }
+  
+  function rerender() {
+    screen.innerText = buffer;
+  }
+  
+  function init() {
+    document
+      .querySelector(".calc-buttons")
+      .addEventListener("click", function (event) {
+        buttonClick(event.target.innerText);
+      });
+  }
+  
+  init();
